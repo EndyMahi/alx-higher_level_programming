8325cf2f2a62ebfa52796cd9673f627ebeddbde7
EndyMahi <endalkeps@gmail.com>
2022-07-19 06:19:08 -0700

    python classes fix

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,13 +1,10 @@
 #!/usr/bin/python3
-"""Define a class Square."""
-
-
 class Square:
-    """Represent a square."""
+    """Represents a square.
+    Private instance attribute: size.
+    Instantiation with size (no type/value verification).
+    """
 
     def __init__(self, size):
-        """Initialize a new Square.
-        Args:
-            size (int): The size of the new square.
-        """
-        self.__size = size 
+        """Initializes the data."""
+        self.__size = size
